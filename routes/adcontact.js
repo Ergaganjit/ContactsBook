92257e99b89a84d99c816212beffea8a5a242a70
harpreetkaur <harpreetkaur04@gmail.com>
2020-04-16 21:26:11 -0400

    added get api in node to fetch data from acontact table which is in mongodb

---
+++ routes/adcontact.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+var mongodb = require("mongodb");
+var MongoClient = mongodb.MongoClient;
+
+/* GET gettting the list of entries from a contacts database. */
+router.get("/", function (req, res, next) {
+  var url = "mongodb://localhost:27017/";
+  var acontact = MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("contacts");
+    dbo
+      .collection("acontact")
+      .find({})
+      .toArray(function (err, result) {
+        if (err) throw err;
+        res.render("acontact", { acontact: result });
+        db.close();
+      });
+  });
+});
+
+module.exports = router;
